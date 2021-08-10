072598b3c6fc40db918a06c6663b5f089b622a0b
jaredg-ai <jaredward.harod@gmail.com>
2021-08-10 13:13:12 -0600

    tables

--- model.py
+++ model.py
@@ -1,3 +1,30 @@
+from operator import index, truediv
+from flask_sqlalchemy import SQLAlchemy
+import correlation
+from collections import defaultdict
 
 
+db = SQLAlchemy()
 
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    email = db.Column(db.String(255), nullable=True)
+    username = db.Column(db.string(255), nullable=True)
+    password = db.Column(db.String(255), nullable=True)
+
+    def __repr__(self):
+
+        return f"<User user_id={self.user_id} email={self.email}"
+
+
+class User_Cards(db.Model):
+
+    __tablename__ = "library"
+
+    User_cards_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    card_id = db.Column(db.Integer, db.ForeignKey('cards.card_id'), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), index=True)
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, flash, redirect, session
+from model import User
+
 
 app = Flask(__name__)
 
@@ -22,13 +24,13 @@ def register_process():
     """Process registration."""
 
     email = request.form["email"]
-    user = request.form["user"]
+    username = request.form["username"]
     password = request.form["password"]
 
-    new_user = User(email=email, user=user, password=password)
+    new_user = User(email=email, username=username, password=password)
 
     flash(f"User {email} added.")
-    return redirect(f"/users/{new_user.user_id}")
+    return redirect(f"/library/{new_user.user_id}")
 
 
 @app.route('/login', methods=['GET'])
@@ -58,7 +60,7 @@ def login_process():
     session["user_id"] = user.user_id
 
     flash("Logged in")
-    return redirect(f"/users/{user.user_id}")
+    return redirect(f"/library/{user.user_id}")
 
 
 @app.route('/logout')
